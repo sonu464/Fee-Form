95c52c7c951178ffbe99d2a4a30b40a35bb590a5
Mr. Kuldeep <128669361+MrKuldeep01@users.noreply.github.com>
2023-09-15 12:22:32 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -7,8 +7,9 @@ const voucher = document.getElementById("voucher");
 const head = document.getElementById("head");
 const date = document.getElementById("date");
 const addParticular = document.getElementById("add-particular");
+const feeType = document.getElementById("fee-type");
+const rsType = document.getElementById("rs-type");
 
-const rupee = document.querySelectorAll(".rupee");
 const rs1 = document.getElementById("rs1");
 const rs2 = document.getElementById("rs2");
 const rs3 = document.getElementById("rs3");
@@ -23,9 +24,57 @@ let dateValue = "";
 
 // Adding particulars
 const particularItem = () => {
-  particularItem.addEventListener("click", () => {
+  addParticular.addEventListener("click", () => {
+    // Adding particular box
     const particularAddingItem = document.createElement("span");
+    particularAddingItem.classList.add("feeOptions");
+    particularAddingItem.innerHTML = `
+         <input type="text" name="Admission fee" />
+      `
+
+    // Adding rupee box
+    const rupeeAddingItem = document.createElement("span");
+    rupeeAddingItem.classList.add("rsOptions")
+    rupeeAddingItem.innerHTML = `
+         <input type="number" class="rupee" min="0" />
+    `
+
+    rsType.appendChild(rupeeAddingItem);
+    feeType.appendChild(particularAddingItem);
+
+    
+// Total Amount
+const totalAmount = () => {
+  const rupee = document.querySelectorAll(".rupee");
+  let total = 0;
+  rupee.forEach((item) => {
+  total += parseFloat(item.value || 0);
+  totalMoney.innerHTML = `
+      <h1>Rs ${total}</h1>
+    `;
+  passesPayment.value = total;
+  paymentType.value = total;
   });
+};
+
+// Adding Event Listener on Rs
+const rupeeTotalAmount =()=>{
+  const rupee = document.querySelectorAll(".rupee");
+  rupee.forEach((item) => {
+    item.addEventListener("input", totalAmount);
+    item.addEventListener("input", () => {
+      if (item.value < 1) {
+        item.value = "";
+      }
+    });
+  });
+}
+
+rupeeTotalAmount();
+
+  });
+
+
 };
 
 // function to check payment by cash or cheque
@@ -43,37 +92,59 @@ const checkMethod = () => {
   }
 };
 
-// Total Amount
-const totalAmount = (item) => {
-  let total = item;
-  total =
-    parseFloat(rs1.value || 0) +
-    parseFloat(rs2.value || 0) +
-    parseFloat(rs3.value || 0) +
-    parseFloat(rs4.value || 0) +
-    parseFloat(rs5.value || 0) +
-    parseFloat(rs6.value || 0);
-  totalMoney.innerHTML = `
-      <h1>Rs ${total}</h1>
-    `;
-  passesPayment.value = total;
-  paymentType.value = total;
-};
+// // Total Amount
+// const totalAmount = () => {
 
-// Adding Event Listener on Rs
-rupee.forEach((item) => {
-  item.addEventListener("input", totalAmount);
-});
+//   let total = 0;
+//   rupee.forEach((item) => {
+//     console.log(item.value);
+//   total += parseFloat(item.value || 0);
+//   totalMoney.innerHTML = `
+//       <h1>Rs ${total}</h1>
+//     `;
+//   passesPayment.value = total;
+//   paymentType.value = total;
+//   });
+// };
+
+// // Adding Event Listener on Rs
+// const rupeeTotalAmount =()=>{
+//   rupee.forEach((item) => {
+//     item.addEventListener("input", totalAmount);
+//     item.addEventListener("input", () => {
+//       if (item.value < 1) {
+//         item.value = "";
+//       }
+  
+//     });
+//   });
+// }
+
+// rupeeTotalAmount();
+
+// Validation Field
+const validation = (callback) => {
+  voucher.addEventListener("input", () => {
+    if (voucher.value < 1) {
+      voucher.value = "";
+    }
+    voucherValue = voucher.value;
+    callback(voucherValue);
+  })
+}
 
 // Function
 // Function to get total value
-totalAmount();
+// totalAmount();
 
 // Function to check the payment method
 checkMethod();
 
 // Function to add particulars
 particularItem();
+
+// Validation
+validation();
 
 // Event Listeners
 // Add an event listener to the select element to listen for changes
